c3720351e360e941978abb5f972032b0c504e3dc
vasya <v.pyskach@gmail.com>
2017-11-30 22:31:46 +0200

    add photo on index

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,5 @@
 class PhotosController < ApplicationController
+	before_action :authenticate_user!
 
 	def index
 		@photos = Photo.all	
@@ -9,21 +10,30 @@ class PhotosController < ApplicationController
 	end
 
 	def create
-		
+		params[:photo][:category_id]=params[:category_id]
+		params[:photo][:user_id]=current_user.id
+		pp @photo=Photo.create(photo_params)
+		redirect_to photos_path
 	end
 
 	def update
-		
+		@photo.update(photo_params)
+  		redirect_to photos_path
 	end
 
 	def destroy
-		
+		@photo.destroy
+  		redirect_to photos_path
 	end
 
 private
 
 	def photo_params
-		
+		params.require(:photo).permit(:user_id, :name, :category_id, :photo)
 	end
 
+	def photo_set
+ 		@photo = Photo.find(params[:id])
+  		
+  	end
 end
